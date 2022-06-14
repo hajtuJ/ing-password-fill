995720b54f43120ab9830c686bffa7e10617b09a
Sebastian Bolek <bolek.sebastian@gmail.com>
2022-06-14 23:37:32 +0200

    Code clean

--- background.js
+++ background.js
@@ -4,41 +4,25 @@ function init() {
 		return string.replace (/[^\d.]/g, '');
 	}
 
-	// navigator.permissions.query({ name: 'clipboard-read' }).then(permission => {
-	// 	if (permission.state === 'denied') {
-	// 		throw new Error('Not allowed to read clipboard.');
-	// 	}
-	// 	navigator.clipboard.read().then(clipboardContents => {
-	// 		const item = clipboardContents[0];
-	// 		if (!item.types.includes('text/plain')) {
-	// 			throw new Error('Clipboard contains non text.');
-	// 		}
-	// 		item.getType('text/plain').then(blob => {
-	// 			blob.text().then(value => {
-					// const arrayPass = value.split('');
+	const pass = document.querySelector('input.ing-password-container__password-autocomplete').value;
+	const arrayPass = pass;
+	const inputs = document.querySelectorAll('div.ing-password-container__password--field input');
 
-					const pass = document.querySelector('input.ing-password-container__password-autocomplete').value;
-					const arrayPass = pass;
-					const inputs = document.querySelectorAll('div.ing-password-container__password--field input');
+	let encodedPass = '';
+	
+	for (let i = 0; i < inputs.length; i++) {
+		
+		const el = inputs[i];
+		const elPos = parseInt(getNumberFromString(el.getAttribute('id')));
+		const passSignPos = elPos - 1;
+		const passLetter = arrayPass[passSignPos];
+		el.nextElementSibling.querySelector('span').innerText = passLetter;
+		encodedPass += passLetter;
+	}
 
-					let encodedPass = '';
-					
-					for (let i = 0; i < inputs.length; i++) {
-						
-						const el = inputs[i];
-						const elPos = parseInt(getNumberFromString(el.getAttribute('id')));
-						const passSignPos = elPos - 1;
-						const passLetter = arrayPass[passSignPos];
-						el.nextElementSibling.querySelector('span').innerText = passLetter;
-						encodedPass += passLetter;
-					}
-
-					//focus first input
-					inputs[0].focus();
-	// 			});
-	// 		});
-	// 	})
-	// });
+	//focus first input
+	inputs[0].focus();
+	
 }
 
 chrome.commands.onCommand.addListener((command) => {
